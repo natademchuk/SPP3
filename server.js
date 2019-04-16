e8c932e51b503b2463e24198f6fc6a4e5f83de7c
Nataliya Demchuk <Nataliya Demchuk>
2019-04-16 11:23:52 +0300

    v2

--- server.js
+++ server.js
@@ -9,25 +9,60 @@ const jsonParser = express.json();
 const fileUpload = require('express-fileupload');
 app.use(fileUpload());
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 app.use('/public', express.static('public'));
 
 let taskInfo = [];
+let userInfo = { userName: "12", userPassword: "12" };
+let SECRET;
 
-app.get('/', function(req, res) {
-    if (taskInfo.length === 0) {
-        res.status(200);
-        res.render("index", { taskAdded: false });
-    } else {
-        res.status(200);
-        res.render("index", {
-            taskAdded: true,
-            taskInfo
-        });
-    }
+let verifyToken = function(req, res, next) {
+    const token = req.cookies.token;
+    jwt.verify(token, SECRET, function(err, decoded) {
+        if (err) {
+            res.status(401);
+        } else {
+            //next();
+            res.status(200);
+        }
+
+    });
+    next();
+};
+
+app.get('/login', function(req, res) { //console.log('req.files >>>', req.files);     // console.log(req.body);
+    taskInfo = [];
+    res.status(200);
+    res.render("login");
     console.log('get success');
 });
 
-app.post('/', urlencodedParser, function(req, res) { //console.log('req.files >>>', req.files);     // console.log(req.body);
+app.post('/login', urlencodedParser, function(req, res) { //console.log('req.files >>>', req.files);     // console.log(req.body);
+    SECRET = Date.now().toString();
+    userInfo.userName = req.body.uName;
+    userInfo.userPassword = req.body.uPassword;
+
+    const token = jwt.sign({ userName: userInfo.userName, userPassword: userInfo.userPassword }, SECRET);
+    res.cookie("token", token);
+    res.sendStatus(200);
+
+    console.log('post success');
+    /*
+     */
+});
+
+app.get('/', verifyToken, function(req, res) {
+    // res.status(200);
+    res.render("index", { taskInfo });
+    //}
+    console.log('get success');
+});
+
+app.post('/', verifyToken, urlencodedParser, function(req, res) { //console.log('req.files >>>', req.files);     // console.log(req.body);
     req.files.myFile.name = req.files.myFile.name.split(' ').join('_');
     let myFile = req.files.myFile;
     let uploadPath = __dirname + '/public/downloadfiles/' + myFile.name;
@@ -36,16 +71,12 @@ app.post('/', urlencodedParser, function(req, res) { //console.log('req.files >>
     let downloadPath = '/public/downloadfiles/' + myFile.name;
     let temptask = { task: req.body.task, status: req.body.status, efdate: req.body.fdate, filename: myFile.name, filepath: downloadPath };
     taskInfo.push(temptask);
-    res.status(200);
-    /*res.render("index", {
-        taskAdded: true,
-        taskInfo
-    });*/
+    //res.status(200);
     res.json(temptask);
     console.log('post success');
 });
 
-app.put('/', urlencodedParser, function(req, res) { //console.log('req.files >>>', req.files); 
+app.put('/', verifyToken, urlencodedParser, function(req, res) { //console.log('req.files >>>', req.files); 
     req.files.myFile.name = req.files.myFile.name.split(' ').join('_');
     let myFile = req.files.myFile;
     let uploadPath = __dirname + '/public/downloadfiles/' + myFile.name;
@@ -54,11 +85,7 @@ app.put('/', urlencodedParser, function(req, res) { //console.log('req.files >>>
     let downloadPath = '/public/downloadfiles/' + myFile.name;
     let temptask = { task: req.body.task, status: req.body.status, efdate: req.body.fdate, filename: myFile.name, filepath: downloadPath };
     taskInfo.splice(req.body.tasknum, 1, temptask);
-    res.status(200);
-    /*res.render("index", {
-        taskAdded: true,
-        taskInfo
-    });*/
+    //res.status(200);
     res.json(temptask);
     console.log('put success');
 
