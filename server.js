74349ccf05c547a0c0d459c99e3426a8f75c135e
Nataliya Demchuk <Nataliya Demchuk>
2019-04-17 21:22:24 +0300

    17.04 21:22

--- server.js
+++ server.js
@@ -16,12 +16,23 @@ app.use(cookieParser());
 
 app.use('/public', express.static('public'));
 
-let taskInfo = [];
+let utasks = []; //данные всех задач для конкретного пользователя
 let SECRET;
 
 const fs = require('fs');
-let users = [];
+let users = []; //данные всех пользователей {username:" ",password:" "}
+let tasks = []; //данные всех задач для каждого пользователя {username:" ", utasks:[]}
+let tempUsername = "";
 
+
+function updateTasks(username, utasks) {
+    let idx = tasks.findIndex(x => x.username === username);
+    tasks.splice(idx, 1, { username, utasks });
+}
+
+function getUTasks(username) {
+    utasks = tasks.find(x => x.username === username).utasks.slice();
+}
 
 let verifyToken = function(req, res, next) {
     const token = req.cookies.token;
@@ -41,7 +52,6 @@ let checkUserRegister = function(req, res, next) {
     } else
         next();
 };
-
 let checkUserLogin = function(req, res, next) {
     if (users.findIndex(x => x.userName === req.body.uName) < 0) {
         res.status(404).end(); //user doesn't exist
@@ -53,10 +63,6 @@ let checkUserLogin = function(req, res, next) {
                 res.status(401).end(); //wrong password
         });
     }
-    /*else if (users.find(x => x.userName === req.body.uName).userPassword != req.body.uPassword) {
-        res.status(401).end(); //wrong password
-    } else
-        next();*/
 };
 
 let register = function(req, res, next) {
@@ -64,9 +70,12 @@ let register = function(req, res, next) {
     bcrypt.hash(req.body.uPassword, saltRounds).then(function(hash) {
         users.push({ userName: req.body.uName, userPassword: hash });
     });
+    tasks.push({ username: req.body.uName, utasks: [] });
     next();
 };
 let login = function(req, res, next) {
+    tempUsername = req.body.uName;
+    getUTasks(req.body.uName);
     SECRET = Date.now().toString();
     let userInfo = { userName: req.body.uName, userPassword: req.body.uPassword };
     const token = jwt.sign({ userName: userInfo.userName, userPassword: userInfo.userPassword }, SECRET);
@@ -75,35 +84,36 @@ let login = function(req, res, next) {
 };
 
 app.get('/register', function(req, res) {
-    //taskInfo = [];
+    //utasks = [];
     res.status(200);
     res.render("autorisation", { title: "REGISTRATION", header: "Registration ", action: "Register" });
     console.log('get /register success');
 });
-
 app.post('/register', urlencodedParser, checkUserRegister, register, login, function(req, res) {
     res.sendStatus(200);
     console.log('post /register success');
 });
+
+
 app.get('/login', function(req, res) {
-    //taskInfo = [];
+    //utasks = [];
     res.status(200);
     res.render("autorisation", { title: "LOGIN", header: "Welcome back ! ", action: "Login" });
     console.log('get /login success');
 });
-
 app.post('/login', urlencodedParser, checkUserLogin, login, function(req, res) {
     res.sendStatus(200);
     console.log('post /login success');
 });
 
+
 app.get('/', verifyToken, function(req, res) {
     //res.status(200);
-    res.render("index", { taskInfo });
+    res.render("index", { utasks });
     console.log('get / success');
 });
 
-app.post('/', verifyToken, urlencodedParser, function(req, res) { //console.log('req.files >>>', req.files);     // console.log(req.body);
+app.post('/', verifyToken, urlencodedParser, function(req, res) {
     req.files.myFile.name = req.files.myFile.name.split(' ').join('_');
     let myFile = req.files.myFile;
     let uploadPath = __dirname + '/public/downloadfiles/' + myFile.name;
@@ -111,13 +121,15 @@ app.post('/', verifyToken, urlencodedParser, function(req, res) { //console.log(
 
     let downloadPath = '/public/downloadfiles/' + myFile.name;
     let temptask = { task: req.body.task, status: req.body.status, efdate: req.body.fdate, filename: myFile.name, filepath: downloadPath };
-    taskInfo.push(temptask);
+    utasks.push(temptask);
     //res.status(200);
     res.json(temptask);
+
+    updateTasks(tempUsername, utasks);
     console.log('post / success');
 });
 
-app.put('/', verifyToken, urlencodedParser, function(req, res) { //console.log('req.files >>>', req.files); 
+app.put('/', verifyToken, urlencodedParser, function(req, res) {
     req.files.myFile.name = req.files.myFile.name.split(' ').join('_');
     let myFile = req.files.myFile;
     let uploadPath = __dirname + '/public/downloadfiles/' + myFile.name;
@@ -125,19 +137,26 @@ app.put('/', verifyToken, urlencodedParser, function(req, res) { //console.log('
 
     let downloadPath = '/public/downloadfiles/' + myFile.name;
     let temptask = { task: req.body.task, status: req.body.status, efdate: req.body.fdate, filename: myFile.name, filepath: downloadPath };
-    taskInfo.splice(req.body.tasknum, 1, temptask);
+    utasks.splice(req.body.tasknum, 1, temptask);
     //res.status(200);
     res.json(temptask);
-    console.log('put / success');
 
+    updateTasks(tempUsername, utasks);
+    console.log('put / success');
 });
 
 process.on('SIGINT', (code) => {
-    let data = JSON.stringify(users, null, 2);
+    let usersdata = JSON.stringify(users, null, 2);
+    let tasksdata = JSON.stringify(tasks, null, 2);
 
-    fs.writeFile(__dirname + '/bd/users_bd.json', data, (err) => {
+    fs.writeFile(__dirname + '/bd/users_bd.json', usersdata, (err) => {
         if (err) throw err;
-        console.log('Data written to file');
+        // console.log('Data written to file');
+    });
+
+    fs.writeFile(__dirname + '/bd/tasks_bd.json', tasksdata, (err) => {
+        if (err) throw err;
+        // console.log('Data written to file');
     });
 
     console.log(`server.js to exit with code: ${code}`);
@@ -153,6 +172,17 @@ const server = app.listen(8000, function() {
             users = JSON.parse(data);
         } catch (e) {
             users = [];
+            //console.log(e);
+        }
+        //console.log(users);
+    });
+
+    fs.readFile(__dirname + '/bd/tasks_bd.json', (err, data) => {
+        if (err) throw err;
+        try {
+            tasks = JSON.parse(data);
+        } catch (e) {
+            tasks = [];
             //console.log(e);
         }
         //console.log(users);
